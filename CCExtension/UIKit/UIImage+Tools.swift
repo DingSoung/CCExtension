eb9f66ba1208f4a86cc152cc4279c0a92fbcd228
Songwen Ding <DingSongwen@haizhi.com>
2016-04-15 19:52:52 +0800

    add image

--- CCExtension/UIKit/UIImage+Tools.swift
+++ CCExtension/UIKit/UIImage+Tools.swift
@@ -10,8 +10,16 @@ import UIKit
 
 extension UIImage {
     
+    /// image to scale
+    public func imageWithScale(scale:CGFloat) -> UIImage {
+        guard let cgImage = self.CGImage else {
+            return self
+        }
+        return UIImage(CGImage: cgImage, scale: scale, orientation: UIImageOrientation.Up)
+    }
+    
     /// scale with ratio
-    public func scaledWithRatio(ratio:CGFloat) -> UIImage {
+    public func imageScaleToRatio(ratio:CGFloat) -> UIImage {
         UIGraphicsBeginImageContext(CGSizeMake(self.size.width * ratio, self.size.height * ratio));
         self.drawInRect(CGRectMake(0, 0, self.size.width * ratio, self.size.height * ratio))
         let scaledImage = UIGraphicsGetImageFromCurrentImageContext();
@@ -20,12 +28,14 @@ extension UIImage {
     }
     
     /// sacale to size
-    public func scaledWithSize(size:CGSize) -> UIImage {
-        UIGraphicsBeginImageContext(size);
+    public func imageScaleToSize(size:CGSize) -> UIImage {
+        // In next line, pass 0.0 to use the current device's pixel scaling factor (and thus account for Retina resolution).
+        // Pass 1.0 to force exact pixel size.
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
         self.drawInRect(CGRectMake(0, 0, size.width, size.height))
-        let scaledImage = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        return scaledImage
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
     }
     
     /// image with cornal radius
