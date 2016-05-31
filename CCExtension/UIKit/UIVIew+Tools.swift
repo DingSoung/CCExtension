9980eabeb610e77465354bf4764b69fe76f77f9a
Songwen Ding <DingSoung@gmail.com>
2016-05-31 13:11:22 +0800

    add fast image capture

--- CCExtension/UIKit/UIVIew+Tools.swift
+++ CCExtension/UIKit/UIVIew+Tools.swift
@@ -10,19 +10,42 @@ import UIKit
 
 extension UIView {
     
-    ///MARK: 设置阴影
+    /// set shadow
     public func setShadow(color:CGColor? = UIColor.blackColor().CGColor, offset:CGSize? = CGSizeMake(0.25, 0.5), opacity:Float? = 0.15, radius:CGFloat? = 1.5) {
         if let v = color { self.layer.shadowColor = v }
         if let v = offset { self.layer.shadowOffset = v }
         if let v = opacity { self.layer.shadowOpacity = v }
         if let v = radius { self.layer.shadowRadius = v }
     }
-    //MARK: 兼容Objective－C
+    
+    /// set shadow Compatible Objective－C
     public func setShadowOC(color:CGColor, offset:CGSize, opacity:Float, radius:CGFloat) {
         self.setShadow(color, offset: offset, opacity: opacity, radius: radius)
     }
     
-    //MARK: 动态全屏  触摸消失
+    /// capture image ,Compatible
+    public var imageCompatible:UIImage? {
+        UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.opaque, 0.0);
+        guard let contex = UIGraphicsGetCurrentContext() else {
+            return nil
+        }
+        self.layer.renderInContext(contex)
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext();
+        return img;
+    }
+    
+    /// capture image much faster
+    public var image:UIImage {
+        UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.opaque, 0.0);
+        // refer http://stackoverflow.com/questions/4334233/how-to-capture-uiview-to-uiimage-without-loss-of-quality-on-retina-display
+        self.drawViewHierarchyInRect(self.bounds, afterScreenUpdates: false)
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext();
+        return img;
+    }
+    
+    /// view in fll screem
     public func viewInFullScreen() {
         UIView.animateWithDuration(1, animations: { () -> Void in
             self.backgroundColor = UIColor(white: 0, alpha: 0.3)
