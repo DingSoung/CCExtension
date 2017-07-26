1cd5427f869f249b740c3072b2b6cb48d227db37
Ding Songwen <DingSoung@gmail.com>
2017-07-26 15:25:20 +0800

    优化Log

--- Extension/Swift+Log.swift
+++ Extension/Swift+Log.swift
@@ -4,20 +4,21 @@
 import Foundation
 
 /// print log with detail message
-@discardableResult public func print(log: Any?,
-                                     file: String = #file,
-                                     line: Int = #line,
-                                     function: String = #function) -> [String : Any] {
-    #if DEBUG
-        print(log ?? "None")
-    #endif
-    return [
+@discardableResult public func log(messgae: Any?,
+                                   file: String = #file,
+                                   line: Int = #line,
+                                   function: String = #function,
+                                   print: Bool? = false) -> [String : Any] {
+    let dict = [
         "message" : log ?? "None",
         "file" : file,
         "line" : line,
         "function" : function,
         "thread" : Thread.current,
         "callstack" : Thread.callStackSymbols,
-    ]
+        ]
+    if print {
+        print(dict)
+    }
+    return dict
 }
-
