8976e42c5a01be73496889babc7c9ca75734666e
Ding Songwen <DingSoung@gmail.com>
2017-09-26 15:51:50 +0800

    support space and background color

--- Extension/UIKit/Array+UIImage.swift
+++ Extension/UIKit/Array+UIImage.swift
@@ -4,8 +4,8 @@
 import UIKit
 
 extension Array where Element: UIImage {
-    
-    public func verticalImage(space:CGFloat) -> UIImage? {
+
+    public func verticalImage(space: CGFloat, backgroundColor: UIColor?) -> UIImage? {
         var size = CGSize.zero
         for image in self {
             let sz = image.size
@@ -13,20 +13,26 @@ extension Array where Element: UIImage {
             size.width = Swift.max(size.width, sz.width)
         }
         size.height += CGFloat(Swift.max(0, self.count - 1)) * space
+        var image: UIImage?
         UIGraphicsBeginImageContextWithOptions(size, false, UIScreen.main.scale)
+        if let color = backgroundColor {
+            let path = UIBezierPath(rect: CGRect(origin: CGPoint.zero, size: size))
+            color.setFill()
+            path.fill()
+        }
         var y:CGFloat = 0
         for image in self {
             image.draw(at: CGPoint(x: 0, y: y))
             y += image.size.height
             y += space
         }
-        let image = UIGraphicsGetImageFromCurrentImageContext()
+        image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext();
         return image;
     }
-    
+
     public var verticalImage: UIImage? {
-        return self.verticalImage(space: 0)
+        return self.verticalImage(space: 0, backgroundColor: nil)
     }
 }
 
--- Extension/UIKit/NSArray+UIImage.swift
+++ Extension/UIKit/NSArray+UIImage.swift
@@ -6,6 +6,10 @@ import Foundation
 @objc
 extension NSArray {
 
+    public func verticalImage(space: CGFloat, backgroundColor: UIColor?) -> UIImage? {
+        return (self as? [UIImage])?.verticalImage(space:space, backgroundColor:backgroundColor)
+    }
+
     public var verticalImage: UIImage? {
         return (self as? [UIImage])?.verticalImage
     }
