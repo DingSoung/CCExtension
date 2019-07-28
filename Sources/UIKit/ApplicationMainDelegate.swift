18b80fb82009646b35890ca70f3d9618a39e9725
Songwen Ding <>
2019-07-28 16:20:52 +0800

    chore: add ApplicationMainDelegate
    Change-Id: I5b74869fe7ec0059ca9252de551a231a9d7d0244

---
+++ Sources/UIKit/ApplicationMainDelegate.swift
@@ -0,0 +1,52 @@
+//  Created by Songwen Ding on 2019/3/21.
+//  Copyright © 2019 Songwen Ding. All rights reserved.
+
+#if canImport(UIKit)
+import UIKit
+
+open class ApplicationMianDelegate: UIResponder {
+    open var modules: [UIApplicationDelegate] {
+        fatalError("override this")
+    }
+}
+
+extension ApplicationMianDelegate: UIApplicationDelegate {
+    open func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        return self.modules.allSatisfy {
+            return $0.application?(application, didFinishLaunchingWithOptions: launchOptions) ?? true
+        }
+    }
+
+    open func applicationWillResignActive(_ application: UIApplication) {
+        self.modules.forEach {
+            $0.applicationWillResignActive?(application)
+        }
+    }
+
+    open func applicationDidEnterBackground(_ application: UIApplication) {
+        self.modules.forEach {
+            $0.applicationDidEnterBackground?(application)
+        }
+    }
+
+    open func applicationWillEnterForeground(_ application: UIApplication) {
+        self.modules.forEach {
+            $0.applicationWillEnterForeground?(application)
+        }
+    }
+
+    open func applicationDidBecomeActive(_ application: UIApplication) {
+        self.modules.forEach {
+            $0.applicationDidBecomeActive?(application)
+        }
+    }
+
+    open func applicationWillTerminate(_ application: UIApplication) {
+        self.modules.forEach {
+            $0.applicationWillTerminate?(application)
+        }
+    }
+}
+#endif
